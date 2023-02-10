e26ba5530f4c41669b717313beee8515d69196f4
frichard <florian.richard@ecole.ensicaen.fr>
2023-02-10 11:35:05 +0100

    ADd responses in components

--- components.go
+++ components.go
@@ -4,4 +4,5 @@ package openapi
 type Components struct {
 	Schemas       Schemas       `json:"schemas,omitempty" yaml:"schemas,omitempty"`
 	RequestBodies RequestBodies `json:"requestBodies,omitempty" yaml:"requestBodies,omitempty"`
+	Responses     Responses     `json:"responses,omitempty" yaml:"responses,omitempty"`
 }
