c050440f1975fabed613272d4782818322e567a9
Florian RICHARD <55540531+FloRichard@users.noreply.github.com>
2023-09-12 17:59:25 +0200

    Update operation.go,n add operationId field

--- operation.go
+++ operation.go
@@ -2,6 +2,7 @@ package openapi
 
 // Operation -.
 type Operation struct {
+	OperationId string 	`json:"operationId,omitempty" yaml:"operationId,omitempty"`
 	Parameters  Parameters  `json:"parameters,omitempty" yaml:"parameters,omitempty"`
 	RequestBody RequestBody `json:"requestBody,omitempty" yaml:"requestBody,omitempty"`
 	Responses   Responses   `json:"responses" yaml:"responses"`
