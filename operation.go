32a4d350cd627f8eb387f2cec8763133a645b53a
Florian RICHARD <55540531+FloRichard@users.noreply.github.com>
2023-09-13 16:05:28 +0200

    Update operation.go

--- operation.go
+++ operation.go
@@ -3,6 +3,7 @@ package openapi
 // Operation -.
 type Operation struct {
 	OperationId string 	`json:"operationId,omitempty" yaml:"operationId,omitempty"`
+	Tags        []string    `json:"tags,omitempty" yaml:"tags,omitempty"`
 	Parameters  Parameters  `json:"parameters,omitempty" yaml:"parameters,omitempty"`
 	RequestBody RequestBody `json:"requestBody,omitempty" yaml:"requestBody,omitempty"`
 	Responses   Responses   `json:"responses" yaml:"responses"`
